5ba21835078cc526ef37f530b28f91e6d29a8621
Sergio Costa <sergio.costa@ufma.br>
2023-05-05 09:40:27 -0300

    update dialog

--- Layer2Triple_main_base_ui.py
+++ Layer2Triple_main_base_ui.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(788, 706)
+        MainWindow.resize(792, 563)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
@@ -252,7 +252,7 @@ class Ui_MainWindow(object):
         self.gridLayout.addLayout(self.formLayout, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 788, 22))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 792, 22))
         self.menubar.setObjectName("menubar")
         self.menuSettings = QtWidgets.QMenu(self.menubar)
         self.menuSettings.setObjectName("menuSettings")
