87575a910017de2ffb9eb7f93ff6913bc1818211
Sergio Costa <sergio.costa@ufma.br>
2023-05-05 08:04:06 -0300

    merge

--- Layer2Triple_main_base_ui.py
+++ Layer2Triple_main_base_ui.py
@@ -330,7 +330,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Layer2Triple"))
         self.label_3.setText(_translate("MainWindow", "Layer"))
         self.button_load_layer.setText(_translate("MainWindow", "Load Layer"))
         self.checkSelected.setText(_translate("MainWindow", "Selected features"))
